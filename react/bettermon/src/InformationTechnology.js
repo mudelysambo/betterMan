61fc39df3d20ccea519e90a4db9ade0a7b6fa975
Naomi2221 <106704080+Naomi2221@users.noreply.github.com>
2023-07-20 20:55:58 +0200

    Pages

---
+++ react/bettermon/src/InformationTechnology.js
@@ -0,0 +1,77 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import './stem.css';
+import Container from "react-bootstrap/Container";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Card  from 'react-bootstrap/Card';
+
+function It(){
+    return(
+    <div>
+          <main role="main" className="pt-5">
+        
+        <Container>
+        <h1> Information Technology </h1> 
+        </Container>
+      
+    </main>
+
+<Container fixed className='maincontain'>
+<Row>
+  <Col>
+<Card className='card'>
+
+  <Card.Img src='https://img.freepik.com/free-vector/science-isometric-concept_1284-16822.jpg?w=740&t=st=1689851387~exp=1689851987~hmac=5501b11d550cbecaf27f58307680a86b6b4d6cdcd98ca4c559b91c78bea12fda' />
+  <Card.Body>
+  <Card.Title>Web Development</Card.Title>
+  <Card.Text>
+View Web Development content
+  </Card.Text>
+  <Button href='/web' variant="primary" role="button" className='p-2'>
+              Enroll&raquo;
+ </Button>
+
+  </Card.Body>
+</Card>
+  </Col>
+  <Col>
+<Card className='card'>
+
+  <Card.Img variant='top' src='https://www.shutterstock.com/image-vector/learning-mathematics-education-knowledge-poster-600w-2109627368.jpg' />
+  <Card.Body>
+  <Card.Title>Business Intelligence</Card.Title>
+  <Card.Text>
+View Business Intelligence content
+  </Card.Text>
+  <Button href='/businessintelli' variant="primary" role="button" className='p-2'>
+              Enroll&raquo;
+            </Button>
+
+  </Card.Body>
+</Card>
+</Col>
+
+<Col>
+<Card className='card'>
+
+  <Card.Img variant='top' src='https://img.freepik.com/free-vector/flat-engineering-construction-illustration_23-2148892787.jpg?w=740&t=st=1689873362~exp=1689873962~hmac=a00f8c4c24364947ccd653dbff10ae0063a8cbaf2ad702b37e94f59e5f34341d' />
+  <Card.Body>
+  <Card.Title>Programming</Card.Title>
+  <Card.Text>
+  View Programming content
+  </Card.Text>
+  <Button href='/' variant="primary" role="button" className='p-2'>
+              Enroll&raquo;
+            </Button>
+  </Card.Body>
+</Card>
+</Col>
+</Row>
+</Container>
+
+    </div>
+
+    );
+}
+export default It;
--- react/bettermon/src/Stem.js
+++ react/bettermon/src/Stem.js
@@ -1,44 +1,77 @@
 import React from 'react';
-//import './stem.css';
-//import { Link } from 'react-router-dom';
+import './stem.css';
 import Button from 'react-bootstrap/Button';
-import Stack from 'react-bootstrap/Stack';
-import Image from 'react-bootstrap/Image';
-import Footer from '../Components/Footer';
-import NavBar from '../Components/NavBar';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Card  from 'react-bootstrap/Card';
+
+
 function Stem(){
+
 return(
   <div>
 
-<header>
-<NavBar />
-</header>
+       <main role="main" className="pt-5">
+        
+          <Container>
+          <h1> Stem Fields </h1> 
+          </Container>
+        
+      </main>
 
+<Container fixed className='maincontain'>
+  <Row>
+    <Col>
+<Card className='card'>
 
-<h1> Stem Fields </h1> 
+    <Card.Img src='https://img.freepik.com/free-vector/science-isometric-concept_1284-16822.jpg?w=740&t=st=1689851387~exp=1689851987~hmac=5501b11d550cbecaf27f58307680a86b6b4d6cdcd98ca4c559b91c78bea12fda' />
+    <Card.Body>
+    <Card.Title>Science</Card.Title>
+    <Card.Text>
+View Science content
+    </Card.Text>
+    <Button href='/science' variant="primary" role="button" className='p-2'>
+                Enroll&raquo;
+   </Button>
 
-<Image src="teacher.png/171x180"/>
+    </Card.Body>
+</Card>
+    </Col>
+    <Col>
+<Card className='card'>
 
-<Stack direction="vertical" gap={1}>
-  <Button href="" variant="primary" size="md" style={{color:'teal'}}>
-    Science
-  </Button>
+    <Card.Img variant='top' src='https://www.shutterstock.com/image-vector/learning-mathematics-education-knowledge-poster-600w-2109627368.jpg' />
+    <Card.Body>
+    <Card.Title>Math</Card.Title>
+    <Card.Text>
+View Math content
+    </Card.Text>
+    <Button href='/math' variant="primary" role="button" className='p-2'>
+                Enroll&raquo;
+              </Button>
 
-  <Button href="" variant="primary" size="md" style={{color:'teal'}}>
-    Engineering
-  </Button>
+    </Card.Body>
+</Card>
+</Col>
 
-  <Button href="" variant="primary" size="md" style={{color:'teal'}}>
-    Math
-  </Button>
+<Col>
+<Card className='card'>
 
-</Stack>
-
-
-<footer> 
-   <Footer />
-</footer>
-
+    <Card.Img variant='top' src='https://img.freepik.com/free-vector/flat-engineering-construction-illustration_23-2148892787.jpg?w=740&t=st=1689873362~exp=1689873962~hmac=a00f8c4c24364947ccd653dbff10ae0063a8cbaf2ad702b37e94f59e5f34341d' />
+    <Card.Body>
+    <Card.Title>Engineering</Card.Title>
+    <Card.Text>
+    View Engineering content
+    </Card.Text>
+    <Button href='/engineering' variant="primary" role="button" className='p-2'>
+                Enroll&raquo;
+              </Button>
+    </Card.Body>
+</Card>
+</Col>
+</Row>
+</Container>
 </div>
 )
 }
