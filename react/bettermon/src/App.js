dabee887a43e69d97d883951c578887a9b8f919b
Naomi2221 <106704080+Naomi2221@users.noreply.github.com>
2023-07-20 21:12:20 +0200

    Updated Pages

--- react/bettermon/src/App.js
+++ react/bettermon/src/App.js
@@ -6,7 +6,14 @@ import Hero from './components/home/hero/hero';
 import Login from './components/home/login';
 import Register from './components/home/register';
 import Courses  from './components/courses/course';
-
+import BusinessIntelli from './Pages/BusinessIntelligence';
+import WebD from './Pages/WebDevelopment';
+import Stem from './Pages/Stem';
+import Math from './Pages/Math';
+import Science from './Pages/Science';
+import Engineering from './Pages/Engineering';
+import It from './Pages/InformationTechnology';
+import About from './Pages/MAbout';
 
 const App = () => {
   return (
@@ -17,7 +24,16 @@ const App = () => {
           <Route path='/hero'element ={Hero} /> 
           <Route path='/login' element={Login} /> 
           <Route path='/register' element={Register} /> 
-          <Route path='/course' element={Courses} /> 
+          <Route path='/course' element={Courses} />
+          <Route path="/stem" element={<Stem />} />
+          <Route path="/science" element={<Science />} />
+          <Route path="/engineering" element={<Engineering />} />
+          <Route path="/math" element={<Math />} />
+          <Route path="/businessintelli" element={<BusinessIntelli />} />
+          <Route path="/it" element={<It />} />
+          <Route path="/web" element={<WebD />} />
+          <Route path="/about" element={<About />} />
+
         </Routes>
       </Router>
     </>
---
+++ react/bettermon/src/Pages/BusinessIntelligence.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import './stem.css';
+import Button from 'react-bootstrap/Button';
+import Container  from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Card  from 'react-bootstrap/Card';
+
+function BusinessIntelli(){
+    return(
+    <div>
+      <main role="main" className="pt-5">
+        
+        <Container>
+        <h1> Business Intelligence</h1> 
+        </Container>
+      
+      </main>
+   
+<Container fixed className='maincontain'>
+<Row>
+    <Col>
+<Card className='card'>
+    <Card.Body>
+    <Card.Title>HTML</Card.Title>
+    <Card.Text>
+View HTML content
+    </Card.Text>
+    <Button href='/science' variant="primary" role="button" className='p-2'>
+                Enroll&raquo;
+   </Button>
+
+    </Card.Body>
+</Card>
+    </Col>
+    <Col>
+<Card className='card'>
+    <Card.Body>
+    <Card.Title>CSS</Card.Title>
+    <Card.Text>
+View HTML content
+    </Card.Text>
+    <Button href='/math' variant="primary" role="button" className='p-2'>
+                Enroll&raquo;
+              </Button>
+
+    </Card.Body>
+</Card>
+</Col>
+
+<Col>
+<Card className='card'>
+    <Card.Body>
+    <Card.Title>Vanilla Javascript</Card.Title>
+    <Card.Text>
+    View Vanilla Javascript content
+    </Card.Text>
+    <Button href='/engineering' variant="primary" role="button" className='p-2'>
+                Enroll&raquo;
+              </Button>
+    </Card.Body>
+</Card>
+</Col>
+
+
+<Col>
+<Card className='card'>
+    <Card.Body>
+    <Card.Title>Javascript Framework</Card.Title>
+    <Card.Text>
+    View Javascript Framework content
+    </Card.Text>
+    <Button href='/engineering' variant="primary" role="button" className='p-2'>
+                Enroll&raquo;
+              </Button>
+    </Card.Body>
+</Card>
+</Col>
+<Col>
+<Card className='card'>
+    <Card.Body>
+    <Card.Title>Node.js</Card.Title>
+    <Card.Text>
+    View Engineering content
+    </Card.Text>
+    <Button href='/engineering' variant="primary" role="button" className='p-2'>
+                Enroll&raquo;
+              </Button>
+    </Card.Body>
+</Card>
+</Col>
+
+</Row>
+
+</Container>
+</div>
+);
+}
+
+export default BusinessIntelli;
